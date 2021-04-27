0ad2caa2806891d3dcb565a1b607337cd7dafb2e
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-04-27 19:25:32 +0200

    Added logging and route functions

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
+
 const app = express();
+
+let topMovies = [
+    {title: 'In Bruges'},
+    {title: 'Head-On'},
+    {title: 'The Intouchables'},
+    {title: 'The Dark Knight'},
+    {title: 'Brokeback Mountain'},
+    {title: 'Matrix'},
+    {title: 'Seven Years In Tibet'},
+    {title: 'Ghost Dog'},
+    {title: 'Walk The Line'},
+    {title: 'Django Unchained'}
+];
+
+app.use(morgan('common'));
+app.use(express.static('public')); 
+
+// Error Handling
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send('Whoopsie, seems like an error occured.');
+});
+
+// GET requests
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.get('/',( req, res) => {
+    res.send('Welcome to my Movie App!')
+});
+
+app.listen(8080, () => {
+    console.log ('Your app is listening on port 8080.');
+});
