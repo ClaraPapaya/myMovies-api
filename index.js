d6b502f3a817c0fa9327606c954a4a572d4d5780
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-09-13 14:00:08 +0200

    Switch off cors

--- index.js
+++ index.js
@@ -17,20 +17,20 @@ const passport = require('passport');
 require('./helpers/passport.js');
 
 // Needed for testing: CORS measure to restrict access from all domains
-// app.use(cors());
+app.use(cors());
 
-let allowedOrigins = ['https://localhost:1234', 'https://localhost:1234/login', 'http://localhost:8080', 'https://allmymovies.herokuapp.com/', 'https://allmymovies.netlify.app', 'http://localhost:8080/login', 'https://allmymovies.herokuapp.com/login'];
+// let allowedOrigins = ['https://localhost:1234', 'https://localhost:1234/login', 'http://localhost:8080', 'https://allmymovies.herokuapp.com/', 'https://allmymovies.netlify.app', 'http://localhost:8080/login', 'https://allmymovies.herokuapp.com/login'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 // To connect to local database
