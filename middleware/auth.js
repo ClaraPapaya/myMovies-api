3721a70965210e3f00e44b4194271916d7eea5a2
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-12-05 18:49:40 +0100

    Add comments for JSDoc

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,20 @@
-const jwtSecret = 'your_jwt_secret'; //This has to be the same key used in the JWTStrategy
-
+const jwtSecret = 'your_jwt_secret';
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
-require('../helpers/passport.js'); // Local passport file
+require('../helpers/passport.js');
+
+/**
+ * function to generate the authentication token to allow users to acces the API 
+ * @param {string} user 
+ * @returns JWTToken
+ */
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret,
     {
-      subject: user.Username, // This is the username encoded in the JWT
-      expiresIn: '7d', //This specifies that the token will expire in 7 days
-      algorithm: 'HS256' //This is the algorithm used to "sign" or encode the values of the JWT
+      subject: user.Username,
+      expiresIn: '7d',
+      algorithm: 'HS256'
     });
 }
 
