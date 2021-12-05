caac14a5ee11e5ee9ad37979721d818a026f55cc
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-12-05 18:58:03 +0100

    Set CORS to open again

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ require('./helpers/passport.js');
 const cors = require('cors');
 let allowedOrigins = ['https://clarapapaya.github.io/myMovies-Angular-client/', 'https://allmymovies.netlify.app', 'https://allmymovies.herokuapp.com/', 'http://localhost:8080'];
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
+  // origin: (origin, callback) => {
+  //   if (!origin) return callback(null, true);
+  //   if (allowedOrigins.indexOf(origin) === -1) {
+  //     let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+  //     return callback(new Error(message), false);
+  //   }
+  //   return callback(null, true);
+  // }
 }));
 
 /* To connect to local database:
